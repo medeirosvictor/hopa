f7287456a8f9478050487804ec79bb673aeecd91
Victor Medeiros <victoramedeiros@gmail.com>
2025-04-27 22:37:34 -0400

    working on apparel

--- src/components/Apparel.jsx
+++ src/components/Apparel.jsx
@@ -1,4 +1,5 @@
 import ApparelImage from "./ApparelImage";
+import { useState } from "react";
 
 function Apparel({ info }) {
     const classes = {
@@ -10,24 +11,43 @@ function Apparel({ info }) {
         type: "text-sm italic",
         id: "underline",
         apparelData: "",
+        apparelDetailsButton:
+            "flex flex-col justify-center items-center cursor-pointer border border-black p-1 lowercase",
     };
+
+    const [showApparelDetails, setShowApparelDetails] = useState(false);
 
     return (
         <div className={classes.container}>
             <ApparelImage image={info.picture} />
-            <div className={classes.apparelData}>
-                <div className={classes.name}>Name: {info.name}</div>
-                <div className={classes.type}>Type: {info.type}</div>
-                <div className={classes.color}>Color: {info.color}</div>
-                <div className={classes.position}>
-                    Position: {info.position}
-                </div>
-                <div>
-                    Has Picture Wearing It:{" "}
-                    {info.hasPictureWearingIt ? "Yes" : "No"}
-                </div>
-                <div>ID: {info.id}</div>
+            <div>
+                <button
+                    className={classes.apparelDetailsButton}
+                    onClick={() => setShowApparelDetails(!showApparelDetails)}
+                >
+                    {showApparelDetails ? "Hide" : "Details"}
+                </button>
             </div>
+            {showApparelDetails ? (
+                <>
+                    <div className={classes.apparelData}>
+                        <div>Edit Apparel Info</div>
+                        <div className={classes.name}>Name: {info.name}</div>
+                        <div className={classes.type}>Type: {info.type}</div>
+                        <div className={classes.color}>Color: {info.color}</div>
+                        <div className={classes.position}>
+                            Position: {info.position}
+                        </div>
+                        <div>
+                            Has Picture Wearing It:{" "}
+                            {info.hasPictureWearingIt ? "Yes" : "No"}
+                        </div>
+                        <div>ID: {info.id}</div>
+                    </div>
+                </>
+            ) : (
+                <></>
+            )}
         </div>
     );
 }
